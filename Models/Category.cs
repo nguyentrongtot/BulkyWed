220304743ad2c4c170a7c531073733607ab0282f
nguyentrongtot <nguyentrongtot.it@gmail.com>
2026-01-11 13:03:22 +0700

    CRUD Operation  and notification in .NET

---
+++ Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWed.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        [Required]
+        [Range(1, 100, ErrorMessage = "Display Order must be between 1 and 100")]
+        [DisplayName("Display Order")]
+        public int DisplayOrder { get; set; }
+    }
+}
